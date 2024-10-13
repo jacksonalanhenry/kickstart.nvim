649021ec50cc455c2d3b4349cdb4268aaf52ad47
Jackson Henry <jhenry11094@gmail.com>
2024-10-13 18:27:10 -0500

    changing colorscheme to something I understand.

--- init.lua
+++ init.lua
@@ -824,21 +824,28 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+  {
+    'EdenEast/nightfox.nvim',
+    priority = 1000, -- Set the priority to ensure it loads before other plugins
 
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+    -- 'init' function is called before the plugin is loaded, use it to set global vars
+    config = function()
+      require('nightfox').setup {
+        options = {
+          transparent = false, -- Disable setting background
+          dim_inactive = true, -- Non focused panes set to alternative background
+          styles = { -- Style to be applied to different syntax groups
+            comments = 'italic', -- Value is any valid attr-list value `:help attr-list`
+            keywords = 'bold', -- Value is any valid attr-list value `:help attr-list`
+          },
+          inverse = { -- Inverse highlight for different types
+            match_paren = true,
+            visual = false,
+            search = false,
+          },
+        },
+      }
+      vim.cmd 'colorscheme nightfox'
     end,
   },
 
